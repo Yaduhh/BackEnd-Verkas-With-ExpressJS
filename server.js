300f936256277f4a50fdb9c896b66cec84536b93
MegaDoor <putra61002@gmail.com>
2026-03-06 09:41:22 +0700

    Refactor

--- server.js
+++ server.js
@@ -39,9 +39,9 @@ const allowedOrigins = [
   'http://127.0.0.1:19006', // iOS Simulator Expo web
   'http://localhost:3000', // Backend (for testing)
   'http://127.0.0.1:3000', // Backend (for iOS simulator)
-  'http://192.168.1.7:8081', // Physical device Metro
+  'http://192.168.1.46:8081', // Physical device Metro
   'http://192.168.1.6:19006', // Physical device Expo web
-  'http://192.168.1.7:3000', // Physical device Backend
+  'http://192.168.1.46:3000', // Physical device Backend
   /^http:\/\/192\.168\.\d+\.\d+:8081$/, // Physical device Metro
   /^http:\/\/192\.168\.\d+\.\d+:19006$/, // Physical device Expo web
   /^http:\/\/192\.168\.\d+\.\d+:3000$/, // Physical device Backend
